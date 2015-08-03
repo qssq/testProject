f087ed94addf88334ceee10d6825ca595ee50518
xungong91 <314568735@qq.com>
2015-08-04 01:41:50 +0800

    Initial Commit

---
+++ TestProject/MacHelper/MacHelper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MacHelper
+//
+//  Created by gongxun on 15/8/4.
+//  Copyright (c) 2015年 gongxun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
